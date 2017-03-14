bf454d83d2646e4b86d4102594699f971b98aebe
anhnguyenk13 <anhmiennguyen96@gmail.com>
2017-03-14 10:26:09 +0700

    Update QuanLyHocSinhGiaoVien.sql

--- QuanLyHocSinhGiaoVien.sql
+++ QuanLyHocSinhGiaoVien.sql
@@ -1,31 +1,20 @@
 ﻿
 
-CREATE DATABASE [QuanLiHocSinhGiaoVien]
+CREATE DATABASE [QuanLyHocSinhGiaoVien]
+GO
 
-USE [QuanLiHocSinhGiaoVien]
+USE [QuanLyHocSinhGiaoVien]
+
+CREATE TABLE DangNhap(
+	[tendangnhap] [nvarchar](20) NULL,
+	[matkhau] [nvarchar](20) NULL
+)
 
 CREATE TABLE MonHoc
 (
 	[mamonhoc] [varchar](10) PRIMARY KEY NOT NULL,
 	[tenmonhoc] [nvarchar](50) NULL,
 	[heso] [int] NULL
-)
-
-CREATE TABLE BangDiem(
-	[hocsinhma] [varchar](10)  PRIMARY KEY NOT NULL,
-	[monhocma] [varchar](20) PRIMARY KEY NOT NULL,
-	[tenmonhoc] [nvarchar](10) NULL,
-	[diemmieng1] [float] NULL,
-	[diemmieng2] [float] NULL,
-	[diem15p] [float] NULL,
-	[diem1tiet] [float] NULL,
-	[diemhetmon] [float] NULL,
-	[diemtrungbinh] [float] NULL
-)
-
-CREATE TABLE DangNhap(
-	[tendangnhap] [varchar](20) NULL,
-	[matkhau] [varchar](20) NULL
 ) 
 
 CREATE TABLE GiaoVien(
@@ -47,8 +36,8 @@ CREATE TABLE GiaoVien(
 CREATE TABLE Lop(
 	[malop] [varchar](10) PRIMARY KEY NOT NULL,
 	[soluong] [int] NULL,
-	[giaovienma] [nvarchar](10) NULL,
-	[namhoc] [nvarchar](15) NULL
+	[namhoc] [nvarchar](15) NULL,
+	[giaovienma] [varchar](10) REFERENCES giaovien(magiaovien),
 )
 
 CREATE TABLE HocSinh(
@@ -67,15 +56,29 @@ CREATE TABLE HocSinh(
 	[lopma] [varchar](10) references lop(malop)
 )
 
+CREATE TABLE BangDiem(
+	[hocsinhma] [varchar](10) NOT NULL REFERENCES hocsinh(mahocsinh),
+	[monhocma] [varchar](10) NOT NULL REFERENCES monhoc(mamonhoc),
+	[tenmonhoc] [nvarchar](10) NULL,
+	[diemmieng1] [float] NULL,
+	[diemmieng2] [float] NULL,
+	[diem15p] [float] NULL,
+	[diem1tiet] [float] NULL,
+	[diemhetmon] [float] NULL,
+	[diemtrungbinh] [float] NULL,
+	PRIMARY KEY (hocsinhma, monhocma)	
+)
+
 CREATE TABLE KetQuaHocTap(
-	[hocsinhma] [varchar](10) PRIMARY KEY NOT NULL,
-	[lopma] [varchar](10) NULL,
+	[hocsinhma] [varchar](10) REFERENCES hocsinh(mahocsinh),
+	[lopma] [varchar](10) REFERENCES lop(malop),
 	[diemtrungbinhki1] [float] NULL,
 	[hoclucki1] [nvarchar](10) NULL,
 	[diemtrungbinhki2] [float] NULL,
 	[hoclucki2] [nvarchar](10) NULL,
 	[diemtrungbinhcanam] [float] NULL,
-	[hocluccanam] [nvarchar](10) NULL
+	[hocluccanam] [nvarchar](10) NULL,
+	PRIMARY KEY (hocsinhma)
 )
 	
 
