a98108344fb4e00bca1519c7e282cd1bb855eafa
anhnguyenk13 <anhmiennguyen96@gmail.com>
2017-03-14 10:02:28 +0700

    Update QuanLyHocSinhGiaoVien.sql

--- QuanLyHocSinhGiaoVien.sql
+++ QuanLyHocSinhGiaoVien.sql
@@ -4,24 +4,32 @@ CREATE DATABASE [QuanLiHocSinhGiaoVien]
 
 USE [QuanLiHocSinhGiaoVien]
 
+CREATE TABLE MonHoc
+(
+	[mamonhoc] [varchar](10) PRIMARY KEY NOT NULL,
+	[tenmonhoc] [nvarchar](50) NULL,
+	[heso] [int] NULL
+)
+
 CREATE TABLE BangDiem(
-	[hocsinhma] [nvarchar](10)  primary key NOT NULL,
-	[masomonhoc] [nvarchar](20) NULL,
+	[hocsinhma] [varchar](10)  PRIMARY KEY NOT NULL,
+	[monhocma] [varchar](20) PRIMARY KEY NOT NULL,
 	[tenmonhoc] [nvarchar](10) NULL,
 	[diemmieng1] [float] NULL,
 	[diemmieng2] [float] NULL,
 	[diem15p] [float] NULL,
 	[diem1tiet] [float] NULL,
 	[diemhetmon] [float] NULL,
-	[diemtrungbinh] [float] NULL,
+	[diemtrungbinh] [float] NULL
+)
 
 CREATE TABLE DangNhap(
-	[tendangnhap] [nvarchar](20) NULL,
-	[matkhau] [nvarchar](20) NULL
+	[tendangnhap] [varchar](20) NULL,
+	[matkhau] [varchar](20) NULL
 ) 
 
 CREATE TABLE GiaoVien(
-	[magiaovien] [nvarchar](10) PRIMARY KEY NOT NULL,
+	[magiaovien] [varchar](10) PRIMARY KEY NOT NULL,
 	[hovaten] [nvarchar](40) NULL,
 	[ngaysinh] [datetime] NULL,
 	[gioitinh] [nvarchar](10) NULL,
@@ -33,12 +41,18 @@ CREATE TABLE GiaoVien(
 	[chucvu] [nvarchar](15) NULL,
 	[chuyenmon] [nvarchar](50) NULL,
 	[tinhtrang] [nvarchar](10) NULL,
-	[luong] [nvarchar](15) NULL,
+	[luong] [nvarchar](15) NULL
+)
 
-(
+CREATE TABLE Lop(
+	[malop] [varchar](10) PRIMARY KEY NOT NULL,
+	[soluong] [int] NULL,
+	[giaovienma] [nvarchar](10) NULL,
+	[namhoc] [nvarchar](15) NULL
+)
 
 CREATE TABLE HocSinh(
-	[mahocsinh] [nvarchar](10) PRIMARY KEY NOT NULL,
+	[mahocsinh] [varchar](10) PRIMARY KEY NOT NULL,
 	[hocvaten] [nvarchar](40) NULL,
 	[ngaysinh] [datetime] NULL,
 	[gioitinh] [nvarchar](10) NULL,
@@ -50,25 +64,18 @@ CREATE TABLE HocSinh(
 	[hotenme] [nvarchar](30) NULL,
 	[nghenghiepme] [nvarchar](20) NULL,
 	[sodienthoai] [nvarchar](12) NULL,
-	[lophoc] [nvarchar](10) NULL,
-	[malop] [nvarchar](10) NULL,
+	[lopma] [varchar](10) references lop(malop)
+)
 
-(
 CREATE TABLE KetQuaHocTap(
-	[hocsinhma] [nvarchar](10) PRIMARY KEY NOT NULL,
+	[hocsinhma] [varchar](10) PRIMARY KEY NOT NULL,
+	[lopma] [varchar](10) NULL,
 	[diemtrungbinhki1] [float] NULL,
 	[hoclucki1] [nvarchar](10) NULL,
 	[diemtrungbinhki2] [float] NULL,
 	[hoclucki2] [nvarchar](10) NULL,
 	[diemtrungbinhcanam] [float] NULL,
-	[hocluccanam] [nvarchar](10) NULL,
-
-(
+	[hocluccanam] [nvarchar](10) NULL
+)
 	
-CREATE TABLE Lop(
-	[malop] [nvarchar](10) PRIMARY KEY NOT NULL,
-	[soluong] [int] NULL,
-	[giaovienchunhiemma] [nvarchar](10) NULL,
-	[namhoc] [nvarchar](15) NULL,
 
-(
